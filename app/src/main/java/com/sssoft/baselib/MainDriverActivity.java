4ef53246df5144a966f8a4a4c4ffbf773d6a112b
Wangyamin <1014538183@qq.com>
2022-04-08 14:58:02 +0800

    1.0.1

---
+++ app/src/main/java/com/sssoft/baselib/MainDriverActivity.java
@@ -0,0 +1,17 @@
+package com.sssoft.baselib;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainDriverActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_driver);
+    }
+}
